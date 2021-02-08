0c058789fd0e56dbf0bb2e09f1a945faf29c8b74
Fabian Vogler <fabian@equivalence.ch>
2021-02-08 14:46:15 +0100

    Remove dev environment

--- src/environments/environment.dev.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-  production: true,
-  environmentWarning: 'This is the dev environment. Changes in this environment might get overwritten.',
-  clientId: 'f125ad2c3fd2',
-  loginURI: 'https://auth.worldskills.dev/oauth/authorize',
-  apiEndpoint: 'https://api.worldskills.dev',
-};
